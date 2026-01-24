667423e5c544a3c0861ba4657d3056296adb86a8
Eryk <eryk.roch.szymanski@gmail.com>
2026-01-25 00:25:12 +0100

    Fixed the 10th exercise

--- 10.hs
+++ 10.hs
@@ -1,8 +1,9 @@
 encode :: Eq a => [a] -> [(Int, a)]
-encode xs = inner [] xs
+encode xs = reverse $ inner [] xs
   where
+    inner :: Eq a => [(Int, a)] -> [a] -> [(Int, a)]
     inner acc [] = acc
+    inner [] (y:ys) = inner [(1, y)] ys
     inner (a:as) (y:ys) = if snd a == y
                           then inner ((fst a + 1, snd a):as) ys
-			  else inner ((y, 1):a:as) ys
-
+                          else inner ((1, y):a:as) ys
